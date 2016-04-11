16e968db654b693b1e6210a61a1cd167e4f8e486
joyvai <iftekharjoy1@gmail.com>
2016-04-11 22:59:03 +0600

    Update dragon.py

--- dragon.py
+++ dragon.py
@@ -1,17 +1,46 @@
+# This program imports two modules. The random module will provide the random.randint()
+# function.. You will also want time-related functions
+# that the time module includes, so line 2 imports the time module.
 
 import random 
 import time
+
+""" 
+Line 14 is a def statement. The def statement defines a new function that you can call later in the
+program. When you define this function, you specify the instructions in its def-block. When you
+call this function, the code inside the def-block executes.
+"""
+# Remember, the def statement doesn’t execute the code.
+# It only defines what code to execute when you call the function.
+
 
 def displayIntro():
 	print "You are in a land full of dragons.In front of you,"
 	print "you see two caves.In one cave, the dragon is friendly"
 	print "and will share his treasure with you.The other dragon"
-	print ()
+	
+"""
+Line 26 defines another function called chooseCave() . This function’s code asks the player
+which cave they want to go in, either 1 or 2.
+
+This function needs to make sure the player typed 1 or 2, and not something else. A loop here will
+keep asking the player until they enter one of these two valid responses. This is called input
+validation.
+
+"""
+
 def chooseCave():
-	cave = ''
-	while cave != '1' and cave != '2':
+	cave = '' # cave is a blank string
+	"""This while loop have two condition.This two condition connected by one boolean operator which name is 'and'.
+	one condition => cave != 1
+     +		and 
+	second condition => cave != 2
+	"""
+	while cave != '1' and cave != '2': # This symbol name is not equal '!='.it's a boolean operator.
+	# Boolean operator checks or uses only for True and False.If the logic is True it's return True.if false it's false
+	# Boolean operators compare values and evaluate to a single Boolean value.
 		print 'Which cave will you go into?(1 or 2)'
-		cave = raw_input()
+		cave = raw_input() # it's taking input from user
 	return cave
 
 def checkCave(chooseCave): 
