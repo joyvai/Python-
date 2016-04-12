e598631b7d5f47259647bfd00cabf223d0d5b502
iftekharul islam <iftekharjoy1@gmail.com>
2016-04-12 20:10:42 +0600

    Added files via upload
    This is coinFlips game.

---
+++ coinFlips.py
@@ -0,0 +1,28 @@
+from random import randint
+print ('I will flip a coin 1000 times.guess how many times it will come up heads.(Press enter to begin)')
+flips = 0
+heads = 0
+while flips < 1000:
+    if randint (0,1) == 1:
+        heads += 1
+    flips += 1
+
+    if flips == 900:
+        print ( '900 flips and there have been ' + str(heads) + 'heads.' )
+    elif flips == 100:
+        print ( ' At 100 tosses,heads has come up ' + str(heads) + 'times so far.' )
+    elif flips == 500:
+        print ( 'Half way done, and heads has come up ' + str (heads) + 'times. ' )
+        
+print ( 'Out of 1000 coin tosses, heads came up ' + str(heads) + '  times!'  )
+print ('Were you close?' )
+
+
+
+
+
+
+
+        
+        
+    
