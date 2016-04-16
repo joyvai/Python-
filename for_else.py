a3d54acaa1a27e0d43a68f9931354e5e5851d19d
iftekharul islam <iftekharjoy1@gmail.com>
2016-04-16 17:36:02 +0600

    Create for_else.py
    This is  a codecademy problem.

---
+++ for_else.py
@@ -0,0 +1,9 @@
+name = ['jpy','joy','jahangir','jolil']
+
+for i in name:
+    if i == 'jolil':
+        print 'hey this is ', i
+        break
+    print 'My name is ',i
+else:
+    print 'this is else statement'
