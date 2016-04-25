a9c5d876f1b3d283a382053835c37d44d8edc247
iftekharul islam <iftekharjoy1@gmail.com>
2016-04-26 00:22:31 +0600

    Create whileLoop.py
    How to use a while loop.

---
+++ whileLoop.py
@@ -0,0 +1,12 @@
+
+def iterable_Val():
+	iterable_value = ['cats','pasta','programming','spam']
+	index = 0
+	length = len(iterable_value)
+	while index < length:
+		thing = iterable_value[index]
+		print 'I really like ' , thing 
+		index += 1
+		
+
+iterable_Val()
