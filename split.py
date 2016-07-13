44aba2db4ef527a9d3eb982d7a89fec574a1c15d
iftekharul islam <iftekharjoy1@gmail.com>
2016-07-13 21:05:09 +0800

    split

---
+++ split.py
@@ -0,0 +1,11 @@
+spam = '''Dear Alice,
+How have you been? I am fine.
+There is a container in the fridge
+that is labeled "Milk Experiment." 
+
+Please do not drink it.
+Sincerely,
+Bob
+'''
+print spam.split('\n')
+
