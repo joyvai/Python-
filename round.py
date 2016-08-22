a2a621762b259424548d092f14b951d7c5a8d21a
iftekharul islam <iftekharjoy1@gmail.com>
2016-08-22 21:04:19 +0600

    Round

---
+++ round.py
@@ -0,0 +1,16 @@
+#round() function take two arguments.You can give one 
+#why i use round()?
+#assume the number is 122323.34343434322323
+#now you only need 122323 what you do now?
+#you use round()
+
+number = 122323.34343434322323
+round(number)
+print number
+
+#if you want two decimal places , then 
+
+round(number,2)
+print number
+
+#That's how it works
