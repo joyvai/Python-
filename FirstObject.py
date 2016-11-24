4cb3f23da3eee0ba68070767e34a32a3e6ab397f
iftekharul islam <iftekharjoy1@gmail.com>
2016-11-25 00:44:20 +0600

    First object

---
+++ FirstObject.py
@@ -0,0 +1,24 @@
+import math
+
+class Point:
+    def move(self,x,y):
+        self.x = 0
+        self.y = 0
+    def reset(self):
+        self.move(0, 0)
+    def calculate_distance(self, other_point):
+        return math.sqrt ((self.x - other_point.x)**2+ (self.y - other_point.y)**2)
+
+# How to use it
+
+point1 = Point ()
+point2 = Point()
+
+point1.reset()
+point2.move(5,0)
+
+print (point2.calculate_distance(point1))
+assert(point2.calculate_distance(point1) == point1.calculate_distance(point2))
+point1.move(7,4)
+print (point1.calculate_distance (point2))
+print (point1.calculate_distance(point1))
