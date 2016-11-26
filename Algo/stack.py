74ddb16f11b8b7ce464617d148bb5ef85a50c9e6
iftekharul islam <iftekharjoy1@gmail.com>
2016-11-26 23:56:22 +0600

    Stack
    Data structure

---
+++ Algo/stack.py
@@ -0,0 +1,23 @@
+class Stack:
+    # create a list.
+    
+    def __init__(self):
+        self.items = []
+        
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self,item):
+        self.items.insert(0, item)
+
+    def pop(self):
+        return self.items.pop()
+    def peek(self):
+        return self.items[0]
+
+
+    def size(self):
+        return len(self.items)
+
+s = Stack()
